bd52e6eabca304c15013001b00bf891ffa6db365
Roman <marinov.dev.88@gmail.com>
2022-08-25 15:06:24 +0300

    create facade

---
+++ Structural/Facade/src/Main.kt
@@ -0,0 +1,7 @@
+fun main(){
+
+    val nums: IntArray = intArrayOf(1,2,3,4,5,6,7,8,9,10)
+    val someAction = SomeAction()
+    val result = someAction.search(nums)
+    println("кол-во четных чисел =" + result)
+}
---
+++ Structural/Facade/src/SomeAction.kt
@@ -0,0 +1,12 @@
+class SomeAction {
+
+    fun search(nums: IntArray) : Int{
+        var count = 0
+
+        for (item in nums) {
+            if (item % 2 == 0) { count ++ }
+        }
+        return count
+    }
+
+}
